d67a28c383f76a821c734c059d1b8397abbee3fa
Lepetit Paul <p.lepettit@gmail.com>
2016-11-28 12:10:49 +0100

    Update dbconfig.inc.php
    Change stackocs_web_1 to ocsinventoryngstack_db_1

--- dbconfig.inc.php
+++ dbconfig.inc.php
@@ -1,7 +1,7 @@
 <?php
 define("DB_NAME", "ocsweb");
-define("SERVER_READ","stackocs_db_1");
-define("SERVER_WRITE","stackocs_db_1");
+define("SERVER_READ","ocsinventoryngstack_db_1");
+define("SERVER_WRITE","ocsinventoryngstack_db_1");
 define("COMPTE_BASE","ocs");
 define("PSWD_BASE","ocs");
 $_SESSION["PSWD_BASE"]="ocs";
