e7a1f18ae091a32348afa752c15882c823e081b3
Paul LEPETIT <palle@MacBook-Pro-de-Palle.local>
2016-11-28 10:13:42 +0100

    Modification dbconfing-inc.php

--- dbconfig.inc.php
+++ dbconfig.inc.php
@@ -1,6 +1,8 @@
 <?php
-$_SESSION["DB_NAME"]="ocsweb";
-$_SESSION["SERVEUR_SQL"]="localhost";
-$_SESSION["COMPTE_BASE"]="ocs";
+define("DB_NAME", "ocsweb");
+define("SERVER_READ","stackocs_db_1");
+define("SERVER_WRITE","stackocs_db_1");
+define("COMPTE_BASE","ocs");
+define("PSWD_BASE","ocs");
 $_SESSION["PSWD_BASE"]="ocs";
 ?>
